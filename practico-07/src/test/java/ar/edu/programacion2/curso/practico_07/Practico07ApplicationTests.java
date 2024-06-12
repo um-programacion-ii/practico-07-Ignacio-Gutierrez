bc020f9cb9a88374f2a44637a631a9144a3ae5fd
Ignacio Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2024-06-11 21:39:10 -0300

    spring initializr

---
+++ practico-07/src/test/java/ar/edu/programacion2/curso/practico_07/Practico07ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.programacion2.curso.practico_07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practico07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
